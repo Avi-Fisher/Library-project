bfbb751d03020adb4ad4ec5b8cad9954ede3757c
Avi Fisher <avifisher82@gmail.com>
2025-11-10 17:36:32 +0200

    fix " self.is_available = True"

--- book/Book.py
+++ book/Book.py
@@ -1,11 +1,11 @@
 
 class Book():
 
-    def __init__(self,title,author,ISBN,is_available):
+    def __init__(self,title,author,ISBN):
         self.title = title
         self.isbn = ISBN
         self.author = author
-        self.is_available = is_available
+        self.is_available = True
 
 
 
