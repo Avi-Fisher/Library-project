be9a96b867fff67b86e722373314486ef1bbe3db
Avi Fisher <avifisher82@gmail.com>
2025-11-10 15:06:17 +0200

    creat the class book in function creat book

---
+++ book/Book.py
@@ -0,0 +1,28 @@
+
+class Book():
+
+    def __init__(self):
+        self.name = None
+        self.type = None
+        self.author = None
+
+
+
+    def creat_book(self):
+
+        self.name = input("enter name book")
+        self.type = input("enter type book")
+        self.author = input("enter author book")
+
+
+
+
+
+
+
+
+
+
+
+
+
