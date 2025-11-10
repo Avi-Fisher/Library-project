2b988e842ab00752580585e83a504154351c715c
Avi Fisher <avifisher82@gmail.com>
2025-11-10 17:38:34 +0200

    add to User class magic matood __str__

--- user/User.py
+++ user/User.py
@@ -1,3 +1,6 @@
+from encodings.punycode import selective_find
+
+
 class User():
 
     def __init__(self,name,id):
@@ -5,6 +8,11 @@ class User():
         self.name= name
         self.id = id
         self.borrowed_books = []
+
+
+    def __str__(self):
+
+        return f"Name: {self.name}, Id: {self.id}, Borrowed books: {self.borrowed_books}"
 
 
 
