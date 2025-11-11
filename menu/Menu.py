22046430f10640080b1ad824991b7d828c7a1915
yossi lerer <ylerer770@gmail.com>
2025-11-11 11:40:20 +0200

    dsf

--- menu/Menu.py
+++ menu/Menu.py
@@ -7,9 +7,30 @@ from io1.Io import Io
 
 class Menu:
 
+
+
     def __init__(self):
 
         self.library = Library()
+
+    def load_library(self):
+
+        load_lib = Io().load_library()
+        books = load_lib["books"]
+        users = load_lib["users"]
+
+        for b in books:
+            book = Book(b["title"], b["isbn"], b["author"], b["is_available"])
+
+            self.library.add_book(book)
+
+        for u in users:
+            user = User(u["name"], u["id"], u["borrowed_books"])
+
+            self.library.add_book(user)
+
+    def save_library(self):
+        Io().save_library(self.library.books, self.library.users)
 
 
     def start(self):
